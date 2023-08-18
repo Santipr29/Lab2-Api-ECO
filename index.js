8f87264165fd901e46ab4f3e4978b55322dc724f
Santiago <santipr2004@gmail.com>
2023-08-17 23:40:36 -0500

    feat: finish -no css

---
+++ index.js
@@ -0,0 +1,107 @@
+let pokeid
+async function getApi(pokeid) {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeid}`)
+    const data = await res.json()
+    Pokemons(data)
+}
+
+
+    const bulbasaurBtn = document.createElement('button')
+    bulbasaurBtn.addEventListener('click', ()=> {
+        pokeid = 1;
+        getApi(pokeid)
+        document.getElementById('Pokedex').innerHTML=""
+    }) 
+    const bulbasaurGif = document.createElement('img')
+    bulbasaurGif.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/1.gif"
+    bulbasaurBtn.appendChild(bulbasaurGif)
+
+
+    const charmanderBtn = document.createElement('button')
+    charmanderBtn.addEventListener('click', () => {
+        pokeid = 4;
+        getApi(pokeid)
+        document.getElementById('Pokedex').innerHTML=""
+    })
+    const charmanderGif = document.createElement('img')
+    charmanderGif.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/4.gif"
+    charmanderBtn.appendChild(charmanderGif)
+
+
+    const squirttleBtn = document.createElement('button')
+    squirttleBtn.addEventListener('click', () => {
+        pokeid = 7;
+        getApi(pokeid)
+        document.getElementById('Pokedex').innerHTML=""
+    })
+    const squirttleGif = document.createElement('img')
+    squirttleGif.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/7.gif"
+    squirttleBtn.appendChild(squirttleGif)
+
+
+    const section = document.createElement('section')
+    section.appendChild(bulbasaurBtn)
+    section.appendChild(charmanderBtn)
+    section.appendChild(squirttleBtn)
+
+
+    document.getElementById('BtnsContainer').appendChild(section)
+
+
+    function devolve (pokemon_id){
+        if (pokemon_id >= 2 && pokemon_id <= 9) {
+            getApi(--pokemon_id)
+        }else {
+            getApi(pokemon_id)
+        }
+    }
+    function evolve(pokemon_id){
+        if (pokemon_id >= 1 && pokemon_id <= 8) {
+            getApi(++pokemon_id)
+        }else{
+            getApi(pokemon_id)
+        }
+    }
+
+
+const Pokemons = (data) => {
+
+const Container = document.createElement('div');
+    
+const section = document.createElement('section');
+section.id = "Card";
+   
+const number = document.createElement('h3');
+number.textContent = `No° ${data.id}`;
+
+const img = document.createElement('img');
+img.src = data.sprites.front_default;
+
+const name = document.createElement('h3');
+name.textContent = data.name;
+
+const devolveBtn = document.createElement('button');
+devolveBtn.textContent = "Devolve";
+devolveBtn.addEventListener('click', () => {
+    devolve(data.id)
+    Container.innerHTML = ""
+    document.getElementById('Pokedex').innerHTML=""
+});
+
+const evolveBtn = document.createElement('button');
+evolveBtn.textContent = "Evolve";
+evolveBtn.addEventListener('click', () => {
+    evolve(data.id)
+    document.getElementById('Pokedex').innerHTML=""
+});
+
+section.appendChild(number);
+section.appendChild(img);
+section.appendChild(name);
+section.appendChild(devolveBtn);
+section.appendChild(evolveBtn);
+
+Container.appendChild(section);
+
+document.getElementById('Pokedex').appendChild(Container);
+}
